3744d0edc1bba6b6b438e1d8c831c0ca6fa86091
undefined <2143511430@qq.com>
2024-11-17 21:37:27 +0800

    修改棋盘尺寸参数

--- src/components/CellSizeSelect/index.vue
+++ src/components/CellSizeSelect/index.vue
@@ -3,12 +3,12 @@
         <div class="text-sm/6 font-medium text-white">棋盘尺寸:</div>
         <select @change="changeSelect" :value="boardSize" autocomplete="country-name"
             class="w-[100px] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:max-w-xs sm:text-sm/6">
-            <option :value="item.value" v-for="(item, index) in sizeList" :key="index">{{ item.value }}</option>
+            <option :value="item.value" v-for="(item, index) in sizeList" :key="index">{{ item.label }}</option>
         </select>
     </div>
 </template>
 <script setup>
-const sizeList = ref([{ value: 440 }, { value: 520 }, { value: 600 }])
+const sizeList = ref([{ value: 440, label: '11 x 11' }, { value: 520, label: '13 x 13' }, { value: 600, label: '15 x 15' }])
 const boardSize = defineModel({ type: Number, default: 440 })
 const changeSelect = (e) => {
     // 改变前 do something
