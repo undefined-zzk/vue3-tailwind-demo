cac70a9a381b879349d9c2b59c9d19aa46a1cc45
undefined <2143511430@qq.com>
2024-11-19 22:55:57 +0800

    添加下棋

--- src/views/gomoku/index.vue
+++ src/views/gomoku/index.vue
@@ -15,10 +15,10 @@
                 <div class="fx flex-col gap-2.5">
                     <button :disabled="!isStart"
                         class="w-[100px] hover:bg-gray-600 hover:border-1 text-sm/6 border border-white rounded disabled:cursor-not-allowed"
-                        @click="stopGame">暂停游戏</button>
+                        @click="stopGame">{{ isStop ? '继续' : '暂停' }}游戏</button>
                     <button :disabled="!isStart"
                         :class="`w-[100px] hover:bg-gray-600 text-sm/6 border border-white rounded disabled:cursor-not-allowed`"
-                        @click="resetGame">重置游戏</button>
+                        @click="resetGame">重置本局</button>
                 </div>
                 <div class="fx flex-col gap-2.5">
                     <CellSizeSelect v-model="boardSize" />
@@ -27,7 +27,7 @@
             </div>
         </header>
         <div class="w-full flex flex-col justify-center items-center flex-1 gap-5">
-            <div class="fx gap-4 font-bold ">
+            <div class="fx gap-4 font-bold select-none">
                 <div>黑方: {{ blackCount }}</div>
                 <div class="text-xl"><span>V</span><span class="text-white">S</span></div>
                 <div class="text-white">白方: {{ whiteCount }}</div>
@@ -38,11 +38,11 @@
                     <SvgIcon :class="{ 'animate-chess': user === opponentBlack }" name="black" width="40" height="40" />
                 </div>
                 <div class="border flex flex-wrap box-content  rounded-sm" :style="computedWH">
-                    <div :style="computeCellWh"
-                        class="flex flex-wrap border items-center justify-center hover:cellhover"
-                        v-for="(item, index) in tttData" :key="item.id">
-                        <SvgIcon v-if="item.type == 'black'" name="black" width="20" height="20" />
-                        <SvgIcon v-else name="white" width="20" height="20" />
+                    <div :style="computeCellWh" class="flex flex-wrap border items-center justify-center"
+                        :class="{ 'hover:cellhover': isStart }" v-for="(item) in tttData" :key="item.id"
+                        @dblclick="playChess(item)">
+                        <SvgIcon v-if="item.type === 'black'" name="black" width="20" height="20" />
+                        <SvgIcon v-if="item.type === 'white'" name="white" width="20" height="20" />
                     </div>
                 </div>
                 <div class="flex flex-col items-center">
@@ -67,7 +67,7 @@
 import { nanoid } from 'nanoid'
 import CellSizeSelect from '@/components/CellSizeSelect/index.vue'
 import ColorSelect from '@/components/ColorSelect/index.vue'
-
+const DOWNCOUNTTIME = 300
 // 黑方
 const opponentBlack = 'black'
 // 白方
@@ -79,13 +79,13 @@ const isStop = ref(false)
 // 表格边框颜色
 const borderColor = ref('#D8B4FE')
 // 棋盘宽高
-const boardSize = ref(440) // 440(11X11) 520(13X13) 600(15X15)
+const boardSize = ref(440) // 440(11X11) 520(13X13) 600(15X15)  
 // 每个格子的宽高
 const cellWH = ref(40)
 // 棋盘数据
 const tttData = ref([])
 // 下棋倒计时长（秒）
-const downCount = ref(30)
+const downCount = ref(DOWNCOUNTTIME)
 // 下棋方
 const user = ref('')
 // 定时器
@@ -93,7 +93,7 @@ const timer = ref(null)
 //路由参数
 const route = useRoute()
 // 积分
-const winIntegral = reactive(new Map([['black', 0], ['white', 0]]))
+const winIntegralMap = reactive(new Map([['black', 0], ['white', 0]]))
 // 标题
 const routeTitle = computed(() => {
     return route.meta.title
@@ -110,11 +110,11 @@ const computeCellWh = computed(() => {
 })
 // 黑方积分总和
 const blackCount = computed(() => {
-    return winIntegral.get(opponentBlack)
+    return winIntegralMap.get(opponentBlack)
 })
 // 白方积分总和
 const whiteCount = computed(() => {
-    return winIntegral.get(opponentWhite)
+    return winIntegralMap.get(opponentWhite)
 })
 // 此刻落子方是否是黑方
 const isBlack = computed(() => {
@@ -122,15 +122,15 @@ const isBlack = computed(() => {
 })
 // 黑方积分加1
 const blackAddOne = () => {
-    let num = winIntegral.get(opponentBlack)
+    let num = winIntegralMap.get(opponentBlack)
     num++
-    winIntegral.set(opponentBlack, num)
+    winIntegralMap.set(opponentBlack, num)
 }
 // 白方积分加1
 const whiteAddOne = () => {
-    let num = winIntegral.get(opponentWhite)
+    let num = winIntegralMap.get(opponentWhite)
     num++
-    winIntegral.set(opponentBlack, num)
+    winIntegralMap.set(opponentBlack, num)
 }
 // 格子个数
 const cellNum = computed(() => {
@@ -142,7 +142,9 @@ const getChessData = () => {
     tttData.value = Array.from({ length: cellNum.value }).map((_, index) => {
         return {
             id: nanoid(),
-            type: index % 2 == 0 ? opponentBlack : opponentWhite, // 用于判断是黑方(black)还是白方(white)
+            type: null,// 用于判断是黑方(black)还是白方(white)
+            index, // 索引
+            chessTime: 0,// 落棋时间
         }
     })
 }
@@ -156,21 +158,56 @@ const clearTime = () => {
     timer.value = null
 }
 
+// 判断五子是否相连
+const chessWin = () => {
+    return false
+}
+
+// 开始下棋
+const playChess = (item) => {
+    // 游戏没开始或者处于暂停或者该格子已有棋子直接return
+    if (!isStart.value || isStop.value || item.type) return
+    if (isBlack.value) {
+        // 黑方下棋
+        item.type = opponentBlack
+        item.chessTime = Date.now()
+        user.value = opponentWhite
+    } else {
+        // 白方下棋
+        item.type = opponentWhite
+        item.chessTime = Date.now()
+        user.value = opponentBlack
+    }
+    console.log(tttData.value)
+    // chessWin()
+    startCutDownCount(DOWNCOUNTTIME)
+}
+
 // 重置
 const resetGame = () => {
-
+    startCutDownCount(DOWNCOUNTTIME)
+    getChessData()
 }
 // 结束
 const overGame = () => {
     isStart.value = false
     user.value = ''
     isStop.value = false
+    downCount.value = DOWNCOUNTTIME
+    winIntegralMap.set(opponentBlack, 0)
+    winIntegralMap.set(opponentWhite, 0)
     clearTime()
 }
 // 暂停
 const stopGame = () => {
-    isStop.value = true
-    clearTime()
+    isStop.value = !isStop.value
+    if (isStop.value) {
+        // 暂停
+        clearTime()
+    } else {
+        // 继续
+        startCutDownCount(downCount.value)
+    }
 }
 
 // 倒计时结束判断输赢
@@ -185,8 +222,9 @@ const DetermineWinnerloserByCutDownCount = () => {
     overGame()
 }
 // 开启倒计时
-const startCutDownCount = () => {
-    downCount.value = 30
+const startCutDownCount = (count = downCount.value) => {
+    clearTime()
+    downCount.value = count
     timer.value = setInterval(() => {
         if (downCount.value <= 0) {
             // 判断输赢
